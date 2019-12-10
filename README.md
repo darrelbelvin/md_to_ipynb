0594a4dab47a937932ba6cb150567cd84bac9133
Darrel Belvin <darrel.belvin@gmail.com>
2019-12-10 13:24:52 -0800

    add script and description

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# git-intro
-This DSI repository introduces adding, committing in git and then pushing to a Github repository.  Students then pair up and collaborate on git and Github, and learn to resolve merge conflicts.
 # md_to_ipynb
+Converts markdown files into Jupyter Notebooks.
+
+md_to_ipynb takes in a markdown file and converts it into a Jupyter Notebook.
+If the output file is not specified the name of the markdown file will be used
+with a .ipynb extension.
+
+## Usage:
+```
+python md_to_ipynb.py (-o -h) <markdown file> <file to output>
+```
+Arguments:
+- markdown file : the file to be converted
+- output file : (optional) name of converted file
+- -h   help
+- -o   attempts to open the converted file after conversion (using xdg-open)
+
+## Hints:
+I suggest placing the script in '~/bin/' then run 
+```
+echo 'alias md_to_python='python ~/bin/md_to_python.py' >> .bash_aliases
+```
+You can then run it from the terminal in any folder by simply calling md_to_python
+
+In order for the -o flag to work, your os needs to know how to directly open .ipynb files. For this purpose I reccomend installing [nbopen](https://github.com/takluyver/nbopen) and following the instructions in the readme to integrate with your file manager.
---
+++ md_to_ipynb.py
@@ -0,0 +1,78 @@
+import sys
+import json
+import subprocess
+import re
+
+regex = r"(^[#]+ )|(^\d. )"
+
+def convert(file_in, file_out):
+    with open(file_in, 'r') as filehandle:
+        lines = filehandle.readlines()
+    
+    lines = ''.join(lines)
+    matches = re.finditer(regex, lines, re.MULTILINE)
+    indices = [match.span()[0] for match in list(matches)]
+    if indices[0] == 0:
+        indices = indices[1:]
+    cells = [lines[indices[i]:indices[i+1]] for i in range(len(indices)-1)]
+    js = {
+        "cells":[{
+                 "cell_type":"markdown",
+                 "metadata":{},
+                 "source":cell
+                 } for cell in cells],
+        "metadata":{},
+        "nbformat":4,
+        "nbformat_minor":2
+    }
+    
+    with open(file_out, 'w') as filehandle:
+        filehandle.writelines(json.dumps(js,indent=4, separators=(',', ': ')))
+
+if __name__ == "__main__":
+    try:
+        args = sys.argv[1:]
+        h = False
+        o = False
+        f1 = ""
+        f2 = ""
+        for arg in args:
+            if arg == '-h':
+                h = True
+            elif arg == '-o':
+                o = True
+            elif arg[0] == '-':
+                print("Unrecognized flag: {}".format(arg))
+            elif f1 == "":
+                f1 = arg
+            elif f2 == "":
+                f2 = arg
+
+        if h:
+            print("""python md_to_ipynb.py -o -h <markdown file> <file to output>
+This command takes in a markdown file and converts it into a Jupyter Notebook.
+If the output file is not specified the name of the markdown file will be used
+with a .ipynb extension.
+
+Arguments:
+  -h   help
+  -o   attempts to open the converted file after conversion (using xdg-open)""")
+        elif f1 == "":
+            print("Missing filename argument")
+        else:
+            if f2 == "":
+                if(f1.endswith(".md")):
+                    f2 = f1[:-3] + ".ipynb"
+                else:
+                    f2 = f1 + ".ipynb"
+            
+            convert(f1, f2)
+
+        if o:
+            bashCommand = f"xdg-open {f2}"
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+    
+    except IOError:
+        print("File not accessible")
